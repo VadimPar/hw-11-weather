f499f526bff1a02e5723535e816083fabcb80b99
Вадим Паращенко <VadimP@live.ru>
2022-09-15 18:24:04 +0300

    add code

--- hw-11-weather/hw-11-weather/Program.cs
+++ hw-11-weather/hw-11-weather/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Specialized;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using static System.Net.WebRequestMethods;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.IO;
+using File = System.IO.File;
+using System.Security.Principal;
+using Newtonsoft.Json;
+using WeatherAPI;
+
+internal class Program
+{
+    private static async Task Main()
+    {
+        Console.WriteLine("Введите название города: ");
+        string city = Console.ReadLine();
+        await WeatherFromAPI.GetWeather(city);
+    }
+}
